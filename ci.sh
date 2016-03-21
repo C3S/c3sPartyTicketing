e7e043b138f8082097854465f99b12994ffcb03e
Christoph Scheid <christoph.scheid@c3s.cc>
2016-03-21 17:07:18 +0100

    add script for continuous integration testing

---
+++ ci.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+#
+# continuous integration shell script to set up the project and run tests
+#
+#
+# apt-get install python-virtualenv
+# sudo apt-get install libxml2-dev libxslt1-dev (needed for pyquery)
+# create a virtualenv, preferrably with the python 2.7 variant:
+virtualenv env
+# update setuptools if neccessary
+env/bin/pip install --upgrade pip
+env/bin/pip install -U setuptools
+# set it up
+# this will take a little while and install all necessary dependencies.
+env/bin/python setup.py develop
+# delete the old database
+rm c3sPartyTicketing.sqlite
+# populate the database
+env/bin/initialize_c3sPartyTicketing_db development.ini
+# prepare for tests
+env/bin/pip install nose coverage pep8 pylint pyflakes pyquery
+#
+# test preparation
+#
+# we use selenium for user interface tests. so we need firefox and xvfb
+# start Xvfband send it to the background: Xvfb :10 &
+# export DISPLAY=:10
+# run the tests
+env/bin/nosetests c3smembership/   --with-coverage --cover-html --with-xunit
+# this is how you can run individial tests:
+#env/bin/nosetests c3smembership/tests/test_webtest.py:FunctionalTests.test_faq_template
+
+# for pyflakes
+find c3spartyticketing -regex '.*.py' ! -regex '.*tests.*'|egrep -v '^./tests/'|xargs env/bin/pyflakes  > pyflakes.log || :
+# for pylint
+rm -f pylint.log
+for f in `find c3spartyticketing -regex '.*.py' ! -regex '.*tests.*'|egrep -v '^./tests/'`; do
+env/bin/pylint --output-format=parseable --reports=y $f >> pylint.log
+done || :
